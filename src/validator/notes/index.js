982152dd544389410e645e0cd7aa1d9f7b81a8e4
hafizdarmawan <hafizdarmawan53@gmail.com>
2022-07-24 02:51:18 +0700

    Postgres databse implemented

--- src/validator/notes/index.js
+++ src/validator/notes/index.js
@@ -6,7 +6,7 @@ const NotesValidator = {
   validateNotePayload: (payload) => {
     const validationResult = NotePayloadSchema.validate(payload);
     if (validationResult.error) {
-      throw new Error(validationResult.error.message);
+      throw new InvariantError(validationResult.error.message);
     }
   },
 };
